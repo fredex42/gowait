95cabc92b2786cbb7140415097d6bf98bd305bbd
Andy Gallagher <andy.gallagher@theguardian.com>
2019-05-14 10:41:19 +0100

    WIP

--- src/watcher/filescanner.go
+++ src/watcher/filescanner.go
@@ -1,27 +1,32 @@
 package main
 
 import "io/ioutil"
+import "github.com/go-redis/redis"
 
 /**
 scans the given directory and returns a list of WatchRecord pointers, updated for
 this pass.
 */
-func scan_pass(dirname string) ([]WatchRecord, error){
+func scan_pass(dirname string, redisClient *redis.Client) ([]*WatchRecord, error){
   files, err := ioutil.ReadDir(dirname)
 
   if(err != nil) {
     return nil, err
   }
 
-  out_record := make([]WatchRecord, len(files))
+  out_records := make([]*WatchRecord, len(files))
 
   for i, f := range(files) {
     //see https://golang.org/pkg/os/#FileInfo for description of "f"'s fields
-    record, watchErr := get_watch_record_with_retry(dirname, f.Name())
+    record, watchErr := get_watch_record_with_retry(dirname, f.Name(), redisClient)
     if(watchErr != nil){
       return nil, watchErr
     }
-    out_record[i] = update_watch_record(record, f)
+    new_record, updateErr := update_watch_record(record, &f, redisClient)
+    out_records[i] = new_record
+    if(updateErr != nil){
+      return nil, updateErr
+    }
   }
-  return out_record, nil
+  return out_records, nil
 }
--- src/watcher/watchrecord.go
+++ src/watcher/watchrecord.go
@@ -1,18 +1,81 @@
+package main
+
+import "encoding/gob"
+import "bytes"
+import "github.com/go-redis/redis"
+import "time"
+import "os"
+
 type WatchRecord struct {
-  path String
-  filename String
+  path string
+  filename string
   last_mtime time.Time
   stable_iterations int
+}
 
+/**
+serialize a watchrecord into a byte string
+*/
+func watchrecord_to_gobstring(record *WatchRecord) (*[]byte,error) {
+  b := bytes.Buffer{}
+  e := gob.NewEncoder(&b)
+  err := e.Encode(record)
+  if(err!=nil){ return nil, err }
+
+  rtn := b.Bytes()
+  return &rtn, nil
+}
+
+/**
+deserialize from byte string to watchrecord
+*/
+func gobstring_to_watchrecord(bytestring *[]byte) (*WatchRecord, error) {
+  record := WatchRecord{}
+  buffer := bytes.NewBuffer(*bytestring)
+
+  decoder := gob.NewDecoder(buffer)
+  err := decoder.Decode(&record)
+  if(err!=nil){
+    return nil, err
+  }
+
+  return &record, nil
+}
+
+func get_keyname(path string, filename string) (string) {
+  var keyname string.Builder
+
+  keyname.WriteString(path)
+  keyname.WriteString(":")
+  keyname.WriteString(filename)
+
+  return keyname.String()
 }
 
 /**
 look up the path in Redis. Return nil if we have no record
 */
-func get_watch_record_with_retry(path string, filename string) (*WatchRecord, error){
-
+func get_watch_record_with_retry(path string, filename string, client *redis.Client) (*WatchRecord, error){
+  keyname := get_keyname(path, filename)
+  
+  val, readErr := client.Get(keyname.String()).Result()
+  if(readErr != nil){
+    if(readErr == redis.Nil){ //no record existed, create a new one
+      newRecord := WatchRecord{}
+      newRecord.path = path
+      newRecord.filename = filename
+      newRecord.last_mtime = nil
+      newRecord.stable_iterations = 0
+      return &newRecord, 0
+    } else {                  //something went kaboom
+      return nil, readErr
+    }
+  } else {                  //we got data
+    newRecord := gobstring_to_watchrecord(val)
+    return &newRecord, 0
+  }
 }
 
-func update_watch_record(old_record *WatchRecord, current_state *os.FileInfo) (*WatchRecord, error){
-
+func update_watch_record(old_record *WatchRecord, current_state *os.FileInfo, client *redis.Client) (*WatchRecord, error){
+  return nil, nil
 }
