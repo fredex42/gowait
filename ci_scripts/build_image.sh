7f45db56420de0ee56e582c67910a75512058602
Andy Gallagher <andy.gallagher@theguardian.com>
2019-05-21 11:04:17 +0100

    working with go modules dependencies, finally

--- ci_scripts/build_image.sh
+++ ci_scripts/build_image.sh
@@ -9,16 +9,14 @@ fi
 
 mkdir -p $BASEPATH/ci_scripts/staging
 
-declare -x GOPATH=${HOME}/go:$BASEPATH
-cd $BASEPATH/src/config
-go test
+go test ./...
 
-cd $BASEPATH/src/watcher
 declare -x GOOS=linux
+cd $BASEPATH/watcher
 go build
 
 mv watcher $BASEPATH/ci_scripts/staging
-cp $BASEPATH/src/config/sampleconfig.yaml $BASEPATH/ci_scripts/staging
+cp $BASEPATH/config/sampleconfig.yaml $BASEPATH/ci_scripts/staging
 
 cd $BASEPATH/ci_scripts
 docker build .
---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+  "io/ioutil"
+  "gopkg.in/yaml.v2"
+)
+
+type Watcher struct {
+  PATH string
+  TIMEOUT int
+  STABLE int
+}
+
+type PersistentStore struct {
+  REDISHOST string
+  REDISDB   int
+  REDISPASS string
+}
+
+type ConfigFile struct {
+  WATCHERS []Watcher
+  REDIS    PersistentStore
+}
+
+func LoadConfig(filename string) (*ConfigFile, error) {
+  configData := ConfigFile{}
+
+  data, err := ioutil.ReadFile(filename)
+  if(err!=nil){
+    return nil, err
+  }
+
+  marshalErr := yaml.Unmarshal([]byte(data), &configData)
+  if(marshalErr!=nil){
+    return nil, marshalErr
+  }
+
+  return &configData, nil
+}
+
+func WatcherFor(path string, config *ConfigFile) (*Watcher, error) {
+  for _,v := range config.WATCHERS {
+    if(v.PATH==path) {
+      return &v, nil
+    }
+  }
+  return nil, nil
+}
